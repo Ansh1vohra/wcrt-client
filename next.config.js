278f496fe0b5bffc302f8ca3ba5ddffe8d6add5d
Ansh1vohra <anshvohra1@gmail.com>
2025-06-11 11:37:14 +0530

    updated Banner and Logo 54524

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     formats: ['image/webp'],
-    domains: [process.env.NEXT_PUBLIC_S3_BUCKET_URL?.replace('https://', '') || 'wcrt-content-images.s3.ap-south-1.amazonaws.com'],
+    domains: [process.env.NEXT_PUBLIC_S3_BUCKET_URL?.replace('https://', '') || 'wcrt-content-images.s3.eu-north-1.amazonaws.com'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_S3_BUCKET_URL?.replace('https://', '') || 'wcrt-content-images.s3.ap-south-1.amazonaws.com',
+        hostname: process.env.NEXT_PUBLIC_S3_BUCKET_URL?.replace('https://', '') || 'wcrt-content-images.s3.eu-north-1.amazonaws.com',
         port: '',
         pathname: '/**',
       },
